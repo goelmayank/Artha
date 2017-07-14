376c73a7f54b994dcee1c432a9a66511abae42b0
Mayank Goel <f2014768@pilani.bits-pilani.ac.in>
2017-07-14 14:20:04 +0530

    Search Implemented

--- WindowsFormsApp1/Search.Designer.cs
+++ WindowsFormsApp1/Search.Designer.cs
@@ -28,20 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Search));
             this.label1 = new System.Windows.Forms.Label();
             this.searchLanguage = new System.Windows.Forms.TextBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label2 = new System.Windows.Forms.Label();
             this.searchWord = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.confirmToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(9, 21);
+            this.label1.Location = new System.Drawing.Point(125, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(121, 16);
             this.label1.TabIndex = 4;
@@ -50,7 +59,7 @@
             // searchLanguage
             // 
             this.searchLanguage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.searchLanguage.Location = new System.Drawing.Point(136, 15);
+            this.searchLanguage.Location = new System.Drawing.Point(252, 3);
             this.searchLanguage.Name = "searchLanguage";
             this.searchLanguage.Size = new System.Drawing.Size(209, 26);
             this.searchLanguage.TabIndex = 3;
@@ -59,16 +68,18 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ContextMenuStrip = this.contextMenuStrip1;
             this.dataGridView1.Location = new System.Drawing.Point(12, 47);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(968, 150);
             this.dataGridView1.TabIndex = 17;
+            this.dataGridView1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dataGridView1_KeyDown);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(363, 21);
+            this.label2.Location = new System.Drawing.Point(479, 9);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(93, 16);
             this.label2.TabIndex = 19;
@@ -77,11 +88,63 @@
             // searchWord
             // 
             this.searchWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.searchWord.Location = new System.Drawing.Point(462, 15);
+            this.searchWord.Location = new System.Drawing.Point(578, 3);
             this.searchWord.Name = "searchWord";
             this.searchWord.Size = new System.Drawing.Size(209, 26);
             this.searchWord.TabIndex = 18;
             this.searchWord.TextChanged += new System.EventHandler(this.searchWord_TextChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copyToolStripMenuItem,
+            this.deleteToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(145, 48);
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+            this.copyToolStripMenuItem.Text = "&Copy";
+            this.copyToolStripMenuItem.Click += new System.EventHandler(this.copyToolStripMenuItem_Click);
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.Delete;
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+            this.deleteToolStripMenuItem.Text = "&Delete";
+            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.confirmToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.ShowItemToolTips = true;
+            this.menuStrip1.Size = new System.Drawing.Size(992, 24);
+            this.menuStrip1.TabIndex = 21;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.A)));
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "&About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // confirmToolStripMenuItem
+            // 
+            this.confirmToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("confirmToolStripMenuItem.Image")));
+            this.confirmToolStripMenuItem.Name = "confirmToolStripMenuItem";
+            this.confirmToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.confirmToolStripMenuItem.Text = "Save";
+            this.confirmToolStripMenuItem.Click += new System.EventHandler(this.confirmToolStripMenuItem_Click);
             // 
             // Search
             // 
@@ -89,14 +152,19 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(992, 209);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.searchWord);
-            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.searchWord);
             this.Controls.Add(this.searchLanguage);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.dataGridView1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Search";
             this.Text = "Search";
+            this.Load += new System.EventHandler(this.Search_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -108,5 +176,11 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox searchWord;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem confirmToolStripMenuItem;
     }
 }
--- WindowsFormsApp1/Search.cs
+++ WindowsFormsApp1/Search.cs
@@ -25,13 +25,18 @@ namespace WindowsFormsApp1
             InitializeComponent();
         }
 
+        private void Search_Load(object sender, EventArgs e)
+        {
+            doc = XDocument.Load(path);
+        }
+
         private void searchWord_TextChanged(object sender, EventArgs e)
         {
             string language = searchLanguage.Text;
             string word = searchWord.Text;
             try
             {
-                string txtxml = "";
+                string txtxml = "<Root>";
                 foreach (var element in XDocument.Load(path).Descendants("Row"))
                 {
                     if (element.Elements(language).SingleOrDefault(p => (string)p.Value == word) !=null)
@@ -39,7 +44,7 @@ namespace WindowsFormsApp1
                         txtxml+= element.ToString();
                     }
                 }
-
+                txtxml += "</Root>";
                 StringReader reader = new StringReader(txtxml);
                 XDocument doc1 = XDocument.Load(reader);
                 var res = doc1.Descendants("Row").ToList();
@@ -56,6 +61,74 @@ namespace WindowsFormsApp1
                 Console.WriteLine(ex.Message);
             }
             
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CopyCode();
+        }
+
+        private void CopyCode()
+        {
+            if (obj.getPrivilege(DataOperations.EmailId) == "Editor")
+            {
+                return;
+            }
+            DataObject d = dataGridView1.GetClipboardContent();
+            Clipboard.SetDataObject(d);
+        }
+
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DeleteCode();
+        }
+
+        private void DeleteCode()
+        {
+            try
+            {
+                if (obj.getPrivilege(DataOperations.EmailId) == "Editor")
+                {
+                    return;
+                }
+                if (dataGridView1.SelectedRows.Count == 0)
+                    MessageBox.Show("No row selected. Click on the left margin to select a row");
+                else
+                {
+                    foreach (DataGridViewRow dr in dataGridView1.SelectedRows)
+                    {
+                        doc.Descendants("Row").Where(x => x.Element("Id").Value == dataGridView1.Rows[dr.Index].Cells[0].Value.ToString()).Remove();
+                        obj.log("Email Id: " + DataOperations.EmailId +
+                            " deleted Conversion Table row no " + dataGridView1.Rows[dr.Index].Cells[0].Value.ToString()
+                            );
+                        dataGridView1.Rows.RemoveAt(dr.Index);
+                        doc.Save(path);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+        }
+
+        private void dataGridView1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.C)
+            {
+                CopyCode();
+            }
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This sample is developed by Mayank Goel, Intern, ABB Pvt. Ltd. Core. Please read the Readme.htm for more details");
+        }
+
+        private void confirmToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
